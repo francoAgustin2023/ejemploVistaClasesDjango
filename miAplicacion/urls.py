d0c1859cd5e997ab66b9a1280dfba5f5403e564e
francoAgustin07 <faluque@udc.edu.ar>
2024-09-19 04:27:40 -0300

    Se avanza bastante, se crean los html y algunas vistas

--- miAplicacion/urls.py
+++ miAplicacion/urls.py
@@ -6,9 +6,15 @@ from django.urls import path
 urlpatterns = [
     #urls a vistas en funciones simples
     path('', views.saludo, name="saludo"),
-    path('base/1',  views.indexHardcodeado, name="hardcode"),
-    path('base/2/<str:nombre>',  views.indexNombreUrl, name="nombreUrl"),
-    path('base/3/', views.indexParametro, name="parametro"),
+    path('prueba/1',  views.indexHardcodeado, name="hardcode"),
+    path('prueba/2/<str:nombre>',  views.indexNombreUrl, name="nombreUrl"), #comodin
+    path('prueba/3/', views.indexParametro, name="parametro"), #parametros
+    #urls a clases en funciones simples
+    path('prueba/index', views.Index.as_view(), name="crearMensajes"),
+    path('prueba/saludar/<str:nombre>', views.SaludarComodin.as_view(), name="comodin"), #comodin
+    path('prueba/saludar/', views.Saludar.as_view(), name="saludarParametros"), #parametros
     #A partir de aca, vistas para consultar al modelo. Se usan clases
-
+    path('aplicacion/base', views.mostrarBase,name="mostrarBase"), #solo para probar, borrar este path despues
+    path('aplicacion', views.mostrarBase,name="mostrarBase"),
+    path('aplicacion/crearMensaje', views.crearMensaje,name="crearMensaje"),
 ]
--- miAplicacion/views.py
+++ miAplicacion/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from .models import Mensaje
+
+#Funciones
 
 def saludo(request): #respuesta HTTP 
     return HttpResponse("Hola mundo!")
@@ -10,16 +13,44 @@ def indexHardcodeado(request): #devuelve un saludo con el nombre hardcodeado
     elemento = {
         'nombre': nombre
     }
-    return render(request, 'base.html', elemento)
+    return render(request, 'prueba.html', elemento)
 
 def indexNombreUrl(request, nombre): #devuelve un saludo con el nombre
-    return render(request, 'base.html', {'nombre':nombre})
+    return render(request, 'prueba.html', {'nombre':nombre})
 
 def indexParametro(request): #obtiene valores de parametros en la url
     nombre = request.GET.get('nombre','desconocido')
-    return render(request, 'base.html', {'nombre':nombre})
+    return render(request, 'prueba.html', {'nombre':nombre})
 
-#A partir de aca se empieza a interactuar con el modelo
-class CrearMensaje(View):
+#Clases
+
+class Index(View):
     def get(self, request):
-        
+        return HttpResponse("aca hay que seguir!")
+    
+class Saludar(View):
+    def get(self, request):
+        nombre = request.GET.get('nombre','desconocido')
+        return render(request, 'prueba.html', {'nombre':nombre})
+    
+class SaludarComodin(View):
+    def get(self, request, nombre):
+        return render(request, 'prueba.html', {'nombre':nombre})
+    
+#A partir de aca se empieza a interactuar con el modelo (se usan funciones y clases)
+
+def mostrarBase(request): #borrar despues y dejar solo el mostrar home
+    return render(request, 'base.html')
+
+def mostrarHome(request):
+    return render(request, 'home.html')
+
+def crearMensaje(request):
+    if request.method == 'GET':
+        return render(request, 'crearMensaje.html')
+    else:
+        texto = request.POST['texto']
+        emisor = request.POST['emisor']
+        receptor = request.POST['receptor']
+        Mensaje.objects.create(texto=texto, emisor=emisor, receptor=receptor)
+        return redirect('base')
